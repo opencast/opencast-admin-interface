6d243e5df07940e841f2fed7091ddab078b876fa
Arne Wilken <Arnei@users.noreply.github.com>
2023-11-20 09:38:15 +0100

    Semicolon app/src/store.ts
    Co-authored-by: Ole Wieners <94838646+owi92@users.noreply.github.com>

--- app/src/store.ts
+++ app/src/store.ts
@@ -84,11 +84,11 @@ const store = configureStore({
 });
 
 // Initialize typescript type inference
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 // Use instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
