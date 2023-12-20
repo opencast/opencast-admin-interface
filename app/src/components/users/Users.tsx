38176916b33e8271c964ee3d0aa008b55baf3745
Arnei <arnewilken@yahoo.de>
2023-12-20 11:06:49 +0100

    Move `usersTableMap` into its own file
    Fixes a "can't access lexical declaration 'X' before initialization" error,
    by resolving a kind of circular dependency.

--- app/src/components/users/Users.tsx
+++ app/src/components/users/Users.tsx
@@ -8,7 +8,7 @@ import TableFilters from "../shared/TableFilters";
 import Table from "../shared/Table";
 import Notifications from "../shared/Notifications";
 import NewResourceModal from "../shared/NewResourceModal";
-import { usersTemplateMap } from "../../configs/tableConfigs/usersTableConfig";
+import { usersTemplateMap } from "../../configs/tableConfigs/usersTableMap";
 import { getTotalUsers } from "../../selectors/userSelectors";
 import { fetchUsers } from "../../thunks/userThunks";
 import {
