095b93a9a716c00903adf32ae7145251e0b64ea9
Julian Kniephoff <kniephoff@elan-ev.de>
2024-05-06 18:14:24 +0200

    Simplify the static file server

--- staticServer.js
+++ staticServer.js
@@ -4,53 +4,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-    "/admin-ng",
-    express.static(path.join(__dirname, "test/app/GET/admin-ng"))
-);
-app.use(
-    "/acl-manager",
-    express.static(path.join(__dirname, "test/app/GET/acl-manager"))
-);
-app.use(
-    "/info",
-    express.static(path.join(__dirname, "test/app/GET/info"))
-);
-app.use(
-    "/services",
-    express.static(path.join(__dirname, "test/app/GET/services"))
-);
-app.use(
-    "/sysinfo",
-    express.static(path.join(__dirname, "test/app/GET/sysinfo"))
-);
-// TODO
-app.use("/staticfiles",
-    express.static(path.join(__dirname, "test/app/POST"))
-);
+app.get("/*", express.static(path.join(__dirname, "test/app/GET")));
 
-app.post("/*", (req, res) => {
-    let filePath = path.join(__dirname,'test/app/' + req.method + req.url);
-    setTimeout(function () {
-        res.status(201);
-        res.sendFile(filePath);
-    }, 1000);
-
+app.post("/*", (req, res, next) => {
+    res.status(201);
+    next();
 });
 
-app.delete("/*", (req, res) => {
-    let filePath = path.join(__dirname, 'test/app/' + req.method + req.url);
-    setTimeout(function() {
+const serveStatic = (req, res) => {
+    const filePath = path.join(__dirname, "test/app", req.method.toUpperCase(), req.url);
+    setTimeout(() => {
         res.sendFile(filePath);
     }, 1000);
-});
+};
 
-app.put("/*", (req, res) => {
-    let filePath = path.join(__dirname, 'test/app/' + req.method + req.url);
-    setTimeout(function () {
-        res.sendFile(filePath);
-    }, 1000);
-});
+for (const method of ["put", "post", "delete"]) {
+    app[method]("/*", serveStatic);
+}
 
 app.listen(port, () => console.log(`Listing on port ${port}`));
-
