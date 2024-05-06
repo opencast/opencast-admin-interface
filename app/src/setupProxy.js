e9331c5b667b7b35c266139c202c567350cb6b3f
Julian Kniephoff <kniephoff@elan-ev.de>
2024-05-06 18:29:56 +0200

    Reformat `setupProxy.js`

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -1,20 +1,20 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-	app.use(
-		[
-			"/api",
-			"/admin-ng",
-			"/acl-manager",
-			"/info",
-			"/services",
-			"/sysinfo",
-			"/staticfiles",
-			"/j_spring_security_check",
-		],
-		createProxyMiddleware({
-			target: "http://localhost:5000",
-			changeOrigin: true,
-		})
-	);
+    app.use(
+        [
+            "/api",
+            "/admin-ng",
+            "/acl-manager",
+            "/info",
+            "/services",
+            "/sysinfo",
+            "/staticfiles",
+            "/j_spring_security_check",
+        ],
+        createProxyMiddleware({
+            target: "http://localhost:5000",
+            changeOrigin: true,
+        })
+    );
 };
