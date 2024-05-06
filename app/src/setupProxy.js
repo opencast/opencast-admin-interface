42da61f25cca5ad2845ea5dd39c354c50d934a07
Julian Kniephoff <kniephoff@elan-ev.de>
2024-05-06 15:53:01 +0200

    Only proxy what we need
    Also add a missing proxy.
    Also remove unused static data.
    Note that this is not a thorough cleanup of the `test` folder.

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -3,24 +3,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
 	app.use(
 		[
+			"/api",
 			"/admin-ng",
 			"/acl-manager",
-			"/i18n",
-			"/blacklist",
-			"/capture-agents",
-			"/email",
-			"/groups",
 			"/info",
-			"/roles",
 			"/services",
 			"/sysinfo",
-			"/workflow",
-			"/img",
-			"/app/styles",
 			"/staticfiles",
-			"/public",
-			"/modules",
-			"/shared",
 			"/j_spring_security_check",
 		],
 		createProxyMiddleware({
--- staticServer.js
+++ staticServer.js
@@ -4,7 +4,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-//Todo: not all serve static links from admin-ui-frontend here
 app.use(
     "/admin-ng",
     express.static(path.join(__dirname, "test/app/GET/admin-ng"))
@@ -14,28 +13,8 @@ app.use(
     express.static(path.join(__dirname, "test/app/GET/acl-manager"))
 );
 app.use(
-    "/blacklist",
-    express.static(path.join(__dirname, "test/app/GET/blacklist"))
-);
-app.use(
-    "/capture-agents",
-    express.static(path.join(__dirname, "test/app/GET/capture-agents"))
-);
-app.use(
-    "/email",
-    express.static(path.join(__dirname, "test/app/GET/email"))
-);
-app.use(
-    "/groups",
-    express.static(path.join(__dirname, "test/app/GET/groups"))
-);
-app.use(
     "/info",
     express.static(path.join(__dirname, "test/app/GET/info"))
-);
-app.use(
-    "/roles",
-    express.static(path.join(__dirname, "test/app/GET/roles"))
 );
 app.use(
     "/services",
@@ -45,30 +24,9 @@ app.use(
     "/sysinfo",
     express.static(path.join(__dirname, "test/app/GET/sysinfo"))
 );
-app.use(
-    "/workflow",
-    express.static(path.join(__dirname, "test/app/GET/workflow"))
-);
-app.use("/app/styles",
-    express.static(path.join(__dirname, "app/src/styles"))
-);
-app.use("/i18n",
-    express.static(path.join(__dirname, "test/app/GET/i18n"))
-);
-app.use("/public",
-    express.static(path.join(__dirname, "app/public"))
-);
+// TODO
 app.use("/staticfiles",
     express.static(path.join(__dirname, "test/app/POST"))
-);
-app.use("/modules",
-    express.static(path.join(__dirname, "app/src/components"))
-);
-app.use("/shared",
-    express.static(path.join(__dirname, "app/src/components"))
-);
-app.use("/img",
-    express.static(path.join(__dirname, "app/src/img"))
 );
 
 app.post("/*", (req, res) => {
