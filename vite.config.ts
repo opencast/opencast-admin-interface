a272240d357a4a145e90d0891553cb476685fd20
Julian Kniephoff <kniephoff@elan-ev.de>
2024-06-03 19:43:37 +0200

    Remove an unused import in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
 
 export default defineConfig({
     base: process.env.PUBLIC_URL || "",
