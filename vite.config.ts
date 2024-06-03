38ce4c6c56a9dc1fbde16962f0a93848f8ee7d22
Julian Kniephoff <kniephoff@elan-ev.de>
2024-06-03 19:11:42 +0200

    Fix indentation in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -15,14 +15,14 @@ export default defineConfig({
         port: Number(process.env.PORT) || 3000,
         proxy: {
             '^/(admin-ng|acl-manager|api|info|services|staticfiles|sysinfo|ui)/.*': {
-              target: process.env.PROXY_TARGET || 'https://develop.opencast.org',
-              changeOrigin: true,
-              secure: false,
-              configure: (proxy, options) => {
-                const username = 'admin';
-                const password = 'opencast';
-                options.auth = process.env.PROXY_AUTH || `${username}:${password}`;
-              },
+                target: process.env.PROXY_TARGET || 'https://develop.opencast.org',
+                changeOrigin: true,
+                secure: false,
+                configure: (proxy, options) => {
+                    const username = 'admin';
+                    const password = 'opencast';
+                    options.auth = process.env.PROXY_AUTH || `${username}:${password}`;
+                },
             },
         },
     },
