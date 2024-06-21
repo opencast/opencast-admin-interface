1e2307187e4994b35bfdf977941fc606d2d2acc5
Ron Lucke <lucke@elan-ev.de>
2024-06-21 11:34:10 +0200

    add toggle button to main menu

--- src/components/shared/MainNav.tsx
+++ src/components/shared/MainNav.tsx
@@ -233,7 +233,10 @@ const MainNav = ({
 
 	return (
 		<>
-			<div className="menu-top" onClick={() => toggleMenu()}>
+			<div className="menu-top" >
+				<button className="button-like-anchor" onClick={() => toggleMenu()}>
+					<i className="fa fa-bars" />
+				</button>
 				{isOpen && (
 					<nav id="roll-up-menu">
 						<div id="nav-container">
