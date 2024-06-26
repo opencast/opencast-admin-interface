431efda21327bbd31d893b10e4816a9ebc2db5d1
Arnei <arnewilken@yahoo.de>
2024-06-26 11:29:38 +0200

    Actually do not disable sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     plugins: [react(), svgr(), viteTsconfigPaths(), preserveDirectives()],
     build: {
         outDir: "build",
-        sourcemap: false,
+        sourcemap: true,
     },
     server: {
         open: true,
