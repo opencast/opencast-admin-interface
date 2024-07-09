8722169635e1b71cdee571a6968c5902d6ed0938
Dennis Benz <dennis.benz@uos.de>
2024-07-09 11:46:26 +0200

    Open scheduling tab for scheduled and recording events

--- src/components/events/partials/EventsStatusCell.tsx
+++ src/components/events/partials/EventsStatusCell.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useAppDispatch } from "../../../store";
 import { Tooltip } from "../../shared/Tooltip";
-import { EventDetailsPage } from "./modals/EventDetails";
 import { Event } from "../../../slices/eventSlice";
 import {
 	fetchWorkflowDetails,
@@ -10,6 +9,8 @@ import {
 	fetchWorkflows,
 	openModal
 } from "../../../slices/eventDetailsSlice";
+import { EventDetailsPage } from "./modals/EventDetails";
+import { hasScheduledStatus } from "../../../utils/eventDetailsUtils";
 
 /**
  * This component renders the status cells of events in the table view
@@ -22,11 +23,16 @@ const EventsStatusCell = ({
 	const { t } = useTranslation();
 	const dispatch = useAppDispatch();
 
-	const openWorkflowOperations = () => {
+	const openStatusModal = () => {
+		// Open scheduling modal for scheduled and recording events
+		if (hasScheduledStatus(row)) {
+			return dispatch(openModal(EventDetailsPage.Scheduling, row));
+		}
+
 		dispatch(fetchWorkflows(row.id)).unwrap()
 			.then(async (workflows) => {
+				// Open workflow overview modal if no workflows available
 				if (!workflows.entries.length) {
-					// Open workflow overview modal
 					return dispatch(openModal(EventDetailsPage.Workflow, row));
 				}
 
@@ -45,7 +51,7 @@ const EventsStatusCell = ({
 		<Tooltip title={t("EVENTS.EVENTS.TABLE.TOOLTIP.STATUS")}>
 			<button
 				className="button-like-anchor crosslink"
-				onClick={() => openWorkflowOperations()}
+				onClick={() => openStatusModal()}
 			>
 				{t(row.displayable_status)}
 			</button>
