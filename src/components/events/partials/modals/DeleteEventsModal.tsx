3f1f08ea3dd4e10dd769cf8b4166afa904e3833e
Arnei <arnewilken@yahoo.de>
2025-01-09 12:50:41 +0100

    Remove unused warning during bulk delete
    This patch removes a warning in the bulk delete
    modal that claims the user is not authorized.
    Removing this for two reasons:
    1. The warning claims to have done an authorization
    check. That check is never performed, but the
    warning is always displayed nonetheless.
    2. Users currently cannot select events if they don't
    have write access on them anyway (they don't
    show up in the events table in the first place),
    so there is no need to perform such an
    authorization check in the first place.

--- src/components/events/partials/modals/DeleteEventsModal.tsx
+++ src/components/events/partials/modals/DeleteEventsModal.tsx
@@ -94,10 +94,6 @@ const DeleteEventsModal = ({
 									<p>{t("BULK_ACTIONS.DELETE_EVENTS_WARNING_LINE1")}</p>
 									<p>{t("BULK_ACTIONS.DELETE_EVENTS_WARNING_LINE2")}</p>
 								</div>
-								{/*todo: only show if scheduling Authorized*/}
-								<div>
-									<p>{t("BULK_ACTIONS.DELETE.EVENTS.UNAUTHORIZED")}</p>
-								</div>
 
 								<div className="full-col">
 									<div className="obj">
