55f9a274f179897595b7346877f82fc94ce8c01e
Arnei <arnewilken@yahoo.de>
2025-01-17 11:06:11 +0100

    Fix role

--- src/components/events/partials/wizards/NewEventWizard.tsx
+++ src/components/events/partials/wizards/NewEventWizard.tsx
@@ -208,7 +208,7 @@ const NewEventWizard: React.FC<{
 										nextPage={nextPage}
 										// @ts-expect-error TS(7006):
 										formik={formik}
-										editAccessRole="ROLE_UI_SERIES_DETAILS_ACL_EDIT"
+										editAccessRole="ROLE_UI_EVENTS_DETAILS_ACL_EDIT"
 										viewUsersAccessRole="ROLE_UI_EVENTS_DETAILS_ACL_USER_ROLES_VIEW"
 										viewNonUsersAccessRole="ROLE_UI_EVENTS_DETAILS_ACL_NONUSER_ROLES_VIEW"
 										initEventAclWithSeriesAcl={initEventAclWithSeriesAcl}
