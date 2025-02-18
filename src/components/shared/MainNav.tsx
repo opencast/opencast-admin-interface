b64a471d204060d5e9064a2119744eee9fecf8d7
Arnei <arnewilken@yahoo.de>
2025-02-18 15:49:40 +0100

    Reduce code duplication in the main navigation
    ...which results in more lines of code, but also more readability.

--- src/components/shared/MainNav.tsx
+++ src/components/shared/MainNav.tsx
@@ -24,140 +24,188 @@ const MainNav = ({
 	const { t } = useTranslation();
 	let navigate = useNavigate();
 
-	const user = useAppSelector(state => getUserInformation(state));
 	const orgProperties = useAppSelector(state => getOrgProperties(state));
 
 	const statisticsEnabled = (orgProperties['admin.statistics.enabled'] || 'false').toLowerCase() === 'true';
 	const themesEnabled = (orgProperties['admin.themes.enabled'] || 'false').toLowerCase() === 'true';
 
 	useHotkeys(
-    availableHotkeys.general.EVENT_VIEW.sequence,
-    () => navigate("/events/events"),
+		availableHotkeys.general.EVENT_VIEW.sequence,
+		() => navigate("/events/events"),
 		{ description: t(availableHotkeys.general.EVENT_VIEW.description) ?? undefined },
-    []
-  );
+		[]
+	);
 
 	useHotkeys(
-    availableHotkeys.general.SERIES_VIEW.sequence,
-    () => navigate("/events/series"),
+		availableHotkeys.general.SERIES_VIEW.sequence,
+		() => navigate("/events/series"),
 		{ description: t(availableHotkeys.general.SERIES_VIEW.description) ?? undefined },
-    []
-  );
+		[]
+	);
 
 	useHotkeys(
-    availableHotkeys.general.MAIN_MENU.sequence,
-    () => toggleMenu(),
+		availableHotkeys.general.MAIN_MENU.sequence,
+		() => toggleMenu(),
 		{ description: t(availableHotkeys.general.MAIN_MENU.description) ?? undefined },
-    [toggleMenu]
-  );
+		[toggleMenu]
+	);
 
 	return (
-		<>
-			<div className="menu-top" >
-				<button className="button-like-anchor" onClick={() => toggleMenu()}>
-					<Tooltip title={t("HOTKEYS.DESCRIPTIONS.GENERAL.MAIN_MENU")} placement={"right"}>
-						<i className="fa fa-bars" />
-					</Tooltip>
-				</button>
-				{isOpen && (
-					<nav id="roll-up-menu">
-						<div id="nav-container">
-							{/* todo: more than one href? how? roles? (see MainNav admin-ui-frontend)*/}
-							{hasAccess("ROLE_UI_NAV_RECORDINGS_VIEW", user) &&
-								(hasAccess("ROLE_UI_EVENTS_VIEW", user) ? (
-									<Link to="/events/events">
-										<Tooltip title={t("NAV.EVENTS.TITLE")} placement={"right"}>
-											<i className="events" />
-										</Tooltip>
-									</Link>
-								) : (
-									hasAccess("ROLE_UI_SERIES_VIEW", user) && (
-										<Link to="/events/series">
-											<Tooltip title={t("NAV.EVENTS.TITLE")} placement={"right"}>
-												<i className="events" />
-											</Tooltip>
-										</Link>
-									)
-								))}
-							{hasAccess("ROLE_UI_NAV_CAPTURE_VIEW", user) &&
-								hasAccess("ROLE_UI_LOCATIONS_VIEW", user) && (
-									<Link
-										to="/recordings/recordings"
-									>
-										<Tooltip title={t("NAV.CAPTUREAGENTS.TITLE")} placement={"right"}>
-											<i
-												className="recordings"
-											/>
-										</Tooltip>
-									</Link>
-								)}
-							{hasAccess("ROLE_UI_NAV_SYSTEMS_VIEW", user) &&
-								(hasAccess("ROLE_UI_JOBS_VIEW", user) ? (
-									<Link to="/systems/jobs">
-										<Tooltip  title={t("NAV.SYSTEMS.TITLE")} placement={"right"}>
-											<i className="systems" />
-										</Tooltip>
-									</Link>
-								) : hasAccess("ROLE_UI_SERVERS_VIEW", user) ? (
-									<Link to="/systems/servers">
-										<Tooltip title={t("NAV.SYSTEMS.TITLE")} placement={"right"}>
-											<i className="systems" />
-										</Tooltip>
-									</Link>
-								) : (
-									hasAccess("ROLE_UI_SERVICES_VIEW", user) && (
-										<Link to="/systems/services">
-											<Tooltip title={t("NAV.SYSTEMS.TITLE")} placement={"right"}>
-												<i className="systems" />
-											</Tooltip>
-										</Link>
-									)
-								))}
-							{hasAccess("ROLE_UI_NAV_ORGANIZATION_VIEW", user) &&
-								(hasAccess("ROLE_UI_USERS_VIEW", user) ? (
-									<Link to="/users/users">
-										<Tooltip title={t("NAV.USERS.TITLE")} placement={"right"}>
-											<i className="users" />
-										</Tooltip>
-									</Link>
-								) : hasAccess("ROLE_UI_GROUPS_VIEW", user) ? (
-									<Link to="/users/groups">
-										<Tooltip title={t("NAV.USERS.TITLE")} placement={"right"}>
-											<i className="users" />
-										</Tooltip>
-									</Link>
-								) : (
-									hasAccess("ROLE_UI_ACLS_VIEW", user) && (
-										<Link to="/users/acls">
-											<Tooltip title={t("NAV.USERS.TITLE")} placement={"right"}>
-												<i className="users" />
-											</Tooltip>
-										</Link>
-									)
-								))}
-							{themesEnabled &&
-								hasAccess("ROLE_UI_NAV_CONFIGURATION_VIEW", user) &&
-								hasAccess("ROLE_UI_THEMES_VIEW", user) && (
-									<Link to="/configuration/themes">
-										<Tooltip title={t("NAV.CONFIGURATION.TITLE")} placement={"right"}>
-											<i className="configuration" />
-										</Tooltip>
-									</Link>
-								)}
-							{statisticsEnabled &&
-								hasAccess("ROLE_UI_NAV_STATISTICS_VIEW", user) &&
-								hasAccess("ROLE_UI_STATISTICS_ORGANIZATION_VIEW", user) && (
-									<Link to="/statistics/organization">
-										<Tooltip title={t("NAV.STATISTICS.TITLE")} placement={"right"}>
-											<i className="statistics" />
-										</Tooltip>
-									</Link>
-								)}
-						</div>
-					</nav>
-				)}
-			</div>
-		</>
+		<div className="menu-top" >
+			<button className="button-like-anchor" onClick={() => toggleMenu()}>
+				<Tooltip title={t("HOTKEYS.DESCRIPTIONS.GENERAL.MAIN_MENU")} placement={"right"}>
+					<i className="fa fa-bars" />
+				</Tooltip>
+			</button>
+			{isOpen && (
+				<nav id="roll-up-menu">
+					<div id="nav-container">
+						{/* todo: more than one href? how? roles? (see MainNav admin-ui-frontend)*/}
+						<MainNavButton
+							accessRole="ROLE_UI_NAV_RECORDINGS_VIEW"
+							links={[
+								{
+									path: "/events/events",
+									accessRole: "ROLE_UI_EVENTS_VIEW",
+									tooltipTitle: "NAV.EVENTS.TITLE",
+									className: "events",
+								},
+								{
+									path: "/events/series",
+									accessRole: "ROLE_UI_SERIES_VIEW",
+									tooltipTitle: "NAV.EVENTS.TITLE",
+									className: "events",
+								},
+							]}
+						/>
+						<MainNavButton
+							accessRole="ROLE_UI_NAV_CAPTURE_VIEW"
+							links={[
+								{
+									path: "/recordings/recordings",
+									accessRole: "ROLE_UI_LOCATIONS_VIEW",
+									tooltipTitle: "NAV.CAPTUREAGENTS.TITLE",
+									className: "recordings",
+								},
+							]}
+						/>
+						<MainNavButton
+							accessRole="ROLE_UI_NAV_SYSTEMS_VIEW"
+							links={[
+								{
+									path: "/systems/jobs",
+									accessRole: "ROLE_UI_JOBS_VIEW",
+									tooltipTitle: "NAV.SYSTEMS.TITLE",
+									className: "systems",
+								},
+								{
+									path: "/systems/servers",
+									accessRole: "ROLE_UI_SERVERS_VIEW",
+									tooltipTitle: "NAV.SYSTEMS.TITLE",
+									className: "systems",
+								},
+								{
+									path: "/systems/services",
+									accessRole: "ROLE_UI_SERVICES_VIEW",
+									tooltipTitle: "NAV.SYSTEMS.TITLE",
+									className: "systems",
+								},
+							]}
+						/>
+						<MainNavButton
+							accessRole="ROLE_UI_NAV_ORGANIZATION_VIEW"
+							links={[
+								{
+									path: "/users/users",
+									accessRole: "ROLE_UI_USERS_VIEW",
+									tooltipTitle: "NAV.USERS.TITLE",
+									className: "users",
+								},
+								{
+									path: "/users/groups",
+									accessRole: "ROLE_UI_GROUPS_VIEW",
+									tooltipTitle: "NAV.USERS.TITLE",
+									className: "users",
+								},
+								{
+									path: "/users/acls",
+									accessRole: "ROLE_UI_ACLS_VIEW",
+									tooltipTitle: "NAV.USERS.TITLE",
+									className: "users",
+								},
+							]}
+						/>
+						{themesEnabled &&
+							<MainNavButton
+								accessRole="ROLE_UI_NAV_CONFIGURATION_VIEW"
+								links={[
+									{
+										path: "/configuration/themes",
+										accessRole: "ROLE_UI_THEMES_VIEW",
+										tooltipTitle: "NAV.CONFIGURATION.TITLE",
+										className: "configuration",
+									},
+								]}
+							/>
+						}
+						{statisticsEnabled &&
+							<MainNavButton
+								accessRole="ROLE_UI_NAV_STATISTICS_VIEW"
+								links={[
+									{
+										path: "/statistics/organization",
+										accessRole: "ROLE_UI_STATISTICS_ORGANIZATION_VIEW",
+										tooltipTitle: "NAV.STATISTICS.TITLE",
+										className: "statistics",
+									},
+								]}
+							/>
+						}
+					</div>
+				</nav>
+			)}
+		</div>
+	);
+};
+
+const MainNavButton = ({
+	accessRole,
+	links,
+}: {
+	accessRole: string
+	links: (React.ComponentProps<typeof MainNavLink> & {accessRole: string})[]
+}) => {
+
+	const user = useAppSelector(state => getUserInformation(state));
+
+	const linkProps = links.find(props => hasAccess(props.accessRole, user));
+
+	return (
+		hasAccess(accessRole, user) &&
+		linkProps &&
+			<MainNavLink
+				{...linkProps}
+			/>
+	);
+}
+
+const MainNavLink = ({
+	path,
+	tooltipTitle,
+	className,
+}: {
+	path: string
+	tooltipTitle: string
+	className: string
+}) => {
+	const { t } = useTranslation();
+
+	return (
+		<Link to={path}>
+			<Tooltip title={t(tooltipTitle)} placement={"right"}>
+				<i className={className} />
+			</Tooltip>
+		</Link>
 	);
 };
 
