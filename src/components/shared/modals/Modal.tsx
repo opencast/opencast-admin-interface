55721a20b45140fa44cb870395f6ce2197da386a
luniki <logseq@luniki.de>
2025-05-13 13:41:55 +0200

    Fix problem with tooltipText.

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -74,7 +74,7 @@ export const Modal = forwardRef<ModalHandle, PropsWithChildren<ModalProps>>(({
 							extraClassName="fa fa-times close-modal"
 							onClick={close}
 							tabIndex={0}
-							tooltipText={t("CLOSE")}
+							tooltipText="CLOSE"
 						/>
 						<h2>
 							{header}
