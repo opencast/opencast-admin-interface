55cc78f6fc412ceddc32bb392a72f879290cc291
janni06 <janni.wielpuetz@gmail.com>
2025-05-21 14:58:55 +0200

    ensure that the CI uses its own server

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,7 +74,6 @@ export default defineConfig({
 	webServer: {
 		command: "npm start",
 		url: "http://127.0.0.1:3000",
-		reuseExistingServer: !process.env.CI,
 		timeout: 120 * 1000, // increase the timeout to 120s (from default 60s) for the server to start
 	},
 });
